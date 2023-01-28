6713693167f0b8a5f579737b26e90327825e5ba6
infinex-exchange <admin@infinex.cc>
2023-01-28 12:34:46 +0100

    Backend ping

--- js/game.js
+++ js/game.js
@@ -2,6 +2,29 @@ $(document).ready(function() {
     window.renderingStagesTarget = 1;
 });
 
+function backendPing() {
+    $.ajax({
+        url: config.apiUrl + '/gamble/session/ping',
+        type: 'POST',
+        data: JSON.stringify({
+            api_key: window.apiKey,
+            gsid: window.gsid,
+            secret: window.secret
+        }),
+        contentType: "application/json",
+        dataType: "json",
+    })
+    .retry(config.retry)
+    .done(function (data) {
+        if(!data.success) {
+            msgBoxRedirect(data.error, true);
+        }
+    })
+    .fail(function (jqXHR, textStatus, errorThrown) {
+        msgBoxNoConn(true);
+    });
+}
+
 $(document).on('authChecked', function() {
     if(!window.loggedIn)
         return;
@@ -33,6 +56,9 @@ $(document).on('authChecked', function() {
         if(data.success) {
             $('.game-name').html(data.name);
             $('#game-frame').attr('src', '/gamble/apps/' + data.frontend + '/');
+            
+            setInterval(backendPing, 30000);
+            
             $(document).trigger('renderingStage');
         } else {
             msgBoxRedirect(data.error, true);
