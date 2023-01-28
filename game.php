95832c4f67814cd147ab1a9d8f153f6475de291d
infinex-exchange <admin@vayamos.cc>
2023-01-28 15:40:54 +0100

    styling

--- game.php
+++ game.php
@@ -26,7 +26,7 @@
                 </div>
                 <div class="col-12 col-lg-auto my-auto">
                     <div class="row">
-                        <div class="col-auto rounded my-auto p-2" style="background-color: var(--color-input)">
+                        <div class="col-auto rounded my-auto px-2 py-1" style="background-color: var(--color-input)">
                             <strong class="game-multiplier small secondary"></strong>
                         </div>
                         <div class="col-auto my-auto">
